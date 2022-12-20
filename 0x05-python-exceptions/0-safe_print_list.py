9943ad78049b293c4dcce3d1733555a486e12f64
eldray <emk.appiah@gmail.com>
2022-12-19 19:43:47 -0800

    initial commit

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    idx = 0
+    while True:
+        try:
+            if idx < x:
+                print(my_list[idx], end='')
+                idx += 1
+            else:
+                print()
+                return idx
+            except IndexError:
+            print()
+            return idx
