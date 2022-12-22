293b9d434724de8802253ed6b3665eab78132983
eldray <emk.appiah@gmail.com>
2022-12-21 21:52:27 -0800

    new

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,47 +1,48 @@
-"""Defines a class Square"""
-
+#!/usr/bin/python3
+"""defines a class square"""
 
 class Square:
-        """Represents a square
-            Attributes:
-                    __size (int): size of a side of the square
-                        """
-                            def __init__(self, size=0):
-                                        """initializes the square
-                                                Args:
-                                                            size (int): size of a side of the square
-                                                                    Returns:
-                                                                                None
-                                                                                        """
+    """Represents a square
+    Attributes:
+    __size (int): size of the sides of a square
+    """
+     def __init__(self, size=0):
+    """Initializes the Square
+    Args:
+    size (int): size of sides of square
+    Returns:
+    None
+    """
                                                                                                 self.size = size
 
                                                                                                     def area(self):
-                                                                                                                """calculates the square's area
+                                                                                                                """Calculates the square's area
                                                                                                                         Returns:
-                                                                                                                                    The area of the square
+                                                                                                                                    Area
                                                                                                                                             """
                                                                                                                                                     return (self.__size) ** 2
 
                                                                                                                                                     @property
                                                                                                                                                         def size(self):
-                                                                                                                                                                    """getter of __size
+                                                                                                                                                                    """getter of size
                                                                                                                                                                             Returns:
-                                                                                                                                                                                        The size of the square
-                                                                                                                                                                                                """
-                                                                                                                                                                                                        return self.__size
+                                                                                                                                                                                       The size of square
+                                                                                                                                                                                               """
+                                                                                                                                                                                                       return self.__size
 
-                                                                                                                                                                                                        @size.setter
-                                                                                                                                                                                                            def size(self, value):
-                                                                                                                                                                                                                        """setter of __size
-                                                                                                                                                                                                                                Args:
-                                                                                                                                                                                                                                            value (int): the size of a size of the square
-                                                                                                                                                                                                                                                    Returns:
-                                                                                                                                                                                                                                                                None
-                                                                                                                                                                                                                                                                        """
-                                                                                                                                                                                                                                                                                if type(value) is not int:
-                                                                                                                                                                                                                                                                                                raise TypeError("size must be an integer")
-                                                                                                                                                                                                                                                                                                    else:
-                                                                                                                                                                                                                                                                                                                    if value < 0:
-                                                                                                                                                                                                                                                                                                                                        raise ValueError("size must be >= 0")
-                                                                                                                                                                                                                                                                                                                                                else:
-                                                                                                                                                                                                                                                                                                                                                                    self.__size = value
+                                                                                                                                                                                                       @size.setter
+                                                                                                                                                                                                           def size(self, value):
+                                                                                                                                                                                                                       """Setter of size
+                                                                                                                                                                                                                               Args:
+                                                                                                                                                                                                                                           value (int): The size of the sides of the square
+                                                                                                                                                                                                                                                   Returns:
+                                                                                                                                                                                                                                                               None
+                                                                                                                                                                                                                                                                       """
+                                                                                                                                                                                                                                                                               if type(value) is not int:
+                                                                                                                                                                                                                                                                                               raise TypeError("size must be an integer")
+                                                                                                                                                                                                                                                                                                   else:
+                                                                                                                                                                                                                                                                                                                   if value < 0:
+                                                                                                                                                                                                                                                                                                                                       raise ValueError("size must be >= 0")
+                                                                                                                                                                                                                                                                                                                                               else:
+                                                                                                                                                                                                                                                                                                                                                                   self.__size = value
+
